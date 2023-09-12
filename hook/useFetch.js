651dfc561c1dd4808980a67b04a5487cdfe7066d
Sharma <shivang.sharma01@sap.com>
2023-09-13 01:38:36 +0530

    fix- first version of Jobster is completed

---
+++ hook/useFetch.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { API_KEY } from "@env";
+
+const useFetch = (endpoint, query) => {
+  const [data, setdata] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setdata(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // for refetching of data in case of any unexpected behaviour
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
